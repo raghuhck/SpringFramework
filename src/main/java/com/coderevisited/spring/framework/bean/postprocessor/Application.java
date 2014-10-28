481f241d4b37b1dec6a98d5a0d17aa9a3255928d
Suresh <mail@sureshsajja.com>
2014-10-29 00:18:28 +0530

    Bean postprocessor

--- src/main/java/com/coderevisited/spring/framework/bean/postprocessor/Application.java
+++ src/main/java/com/coderevisited/spring/framework/bean/postprocessor/Application.java
@@ -2,6 +2,9 @@ package com.coderevisited.spring.framework.bean.postprocessor;
 
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+/**
+ * Demonstration of bean postprocessor
+ */
 public class Application
 {
     public static void main(String[] args)
